0e24bbed59c87b4b59aae4ba63554954567417e7
fxbd5684 <136948328+fxbd5684@users.noreply.github.com>
2024-06-05 18:32:13 +0800

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<!-- 这是一个文档搜索项目 -->
+
+<script>
+    import Doc from "./Doc.svelte";
+    import docs from "./data.json";
+
+
+    let itext = "";
+
+    $: targets = docs.filter(
+        (doc) =>
+            doc.title.toLowerCase().includes(itext.toLowerCase()) ||
+            doc.content.toLowerCase().includes(itext.toLowerCase()),
+    );
+</script>
+
+<input type="text" bind:value={itext} />
+<ul>
+    {#each targets as target}
+        <Doc>
+            <span slot="title">{target.title}</span>
+            <span slot="content">{target.content.substring(0, 120)} ...</span>
+        </Doc>
+    {/each}
+</ul>
---
+++ src/Doc.svelte
@@ -0,0 +1,27 @@
+<div class="doc-box">
+    <h1 class="doc-title"><slot name="title" /></h1>
+    <br />
+    <p class="doc-content"><slot name="content" /></p>
+</div>
+
+<style lang="scss">
+    .doc-box {
+        padding: 12px;
+        border: 1px solid;
+        border-image: linear-gradient(666deg, #a255ff, #5a9cff) 1;
+        border-radius: 12px;
+        margin: 12px;
+        max-width: 500px;
+
+        .doc-content {
+            color: #b6b7d4;
+        }
+
+        .doc-title {
+            font-size: 24px;
+            font-weight: bold;
+            margin-bottom: -20px;
+            color: #ececec;
+        }
+    }
+</style>
